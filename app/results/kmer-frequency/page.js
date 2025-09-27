f7b3accdd0a3f95ee2b2938a8cbf70d99edd3d48
Ashu <ashu36329@gmail.com>
2025-09-27 15:05:47 +0530

    #

--- app/results/kmer-frequency/page.js
+++ app/results/kmer-frequency/page.js
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useRef } from "react";
 import { motion } from "framer-motion";
-import Image from "next/image";
 
 export default function KmerFrequencyPage() {
   const [sequence, setSequence] = useState("");
